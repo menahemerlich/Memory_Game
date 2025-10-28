714329b467aa97e5a20d592f01bcca270f98cbb9
menahem_erlich <erlichmenahem@gmail.com>
2025-10-28 12:08:09 +0200

    I finished function 'board_with_pairs' and it works.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from memory.game import create_board, create_pairs
-
-board_game = create_board(4)
-for i in board_game:
+from memory.game import create_board, create_pairs, board_with_pairs
+size = 4
+board_game = create_board(size)
+card_list = create_pairs(size)
+board_game_2 = board_with_pairs(board_game, card_list)
+for i in board_game_2:
     for j in i:
         print(j, end=" ")
     print()
 
-print(create_pairs(4))
+
 
--- memory/game.py
+++ memory/game.py
@@ -9,9 +9,19 @@ def create_board(size: int, fill:str = "x"):
                 board[i].append(fill)
     return board
 
-
 def create_pairs(size:int):
-    card_number = []
+    card_list = []
     for i in range(0, (size ** 2) // 2):
-        card_number.append(i + 1)
-    return card_number
+        card_list.append(i + 1)
+        card_list.append(i + 1)
+    return card_list
+
+def board_with_pairs(board: list[list[int]] ,card_list: list[int]):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            mix_card = card_list.pop(0)
+            board[i][j] = mix_card
+    return board
+
+
+
