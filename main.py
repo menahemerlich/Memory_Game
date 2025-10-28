7f7ff4cd05aff2bb8af7210665d9587912f13738
menahem_erlich <erlichmenahem@gmail.com>
2025-10-28 11:32:11 +0200

    I finished function 'create_pairs' and it works.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from memory.game import create_board
+from memory.game import create_board, create_pairs
 
-board_game= create_board(10)
+board_game = create_board(4)
 for i in board_game:
     for j in i:
         print(j, end=" ")
     print()
+
+print(create_pairs(4))
+
--- memory/game.py
+++ memory/game.py
@@ -1,8 +1,17 @@
+import random
 
 def create_board(size: int, fill:str = "x"):
     board = []
-    for i in range(size):
-        board.append([])
-        for j in range(size):
-            board[i].append(fill)
+    if (size ** 2) % 2 == 0:
+        for i in range(size):
+            board.append([])
+            for j in range(size):
+                board[i].append(fill)
     return board
+
+
+def create_pairs(size:int):
+    card_number = []
+    for i in range(0, (size ** 2) // 2):
+        card_number.append(i + 1)
+    return card_number
