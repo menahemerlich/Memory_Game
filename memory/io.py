0f8d4b8b46350f5ce94294cb7776647565788906
menahem_erlich <erlichmenahem@gmail.com>
2025-10-28 15:22:31 +0200

    I finished function 'input_conver' and it works.

--- memory/io.py
+++ memory/io.py
@@ -1,16 +1,21 @@
-correct_pairs = [["2", "5"]]
 def input_coordinates():
     global coordinates
     choice = input("Enter your coordinates: ")
     coordinates = choice.split(" ")
     return coordinates
 
-def test_input(coordinates: list[str], size:int, correct_pairs):
-    for i in coordinates:
-        if i.isnumeric() and coordinates not in correct_pairs and len(coordinates) == 2:
-            if int(i) < size and int(coordinates[-1]) < size:
-                return True
+def test_input(coordinates: list[str], size:int, correct_pairs:list[list[str]]):
+    if coordinates[0].isnumeric() and coordinates[-1].isnumeric() and coordinates not in correct_pairs:
+        if int(coordinates[0]) < size and int(coordinates[-1]) < size and len(coordinates) == 2:
+            return True
     return False
+
+def input_conver(coordinates:list[str]):
+    for i in range(len(coordinates)):
+        coordinates[i] = int(coordinates[i])
+    return coordinates
+
+
 
 
 
